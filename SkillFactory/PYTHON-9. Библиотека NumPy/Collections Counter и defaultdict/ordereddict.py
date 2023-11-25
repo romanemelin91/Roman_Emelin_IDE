1a8c01ddbe7e17b440e7de86129858364fd08309
Roman Emelin <roman.emelin91@gmail.com>
2023-11-25 18:53:05 +0100

    add python files

--- SkillFactory/PYTHON-9. Библиотека NumPy/Collections Counter и defaultdict/ordereddict.py
+++ SkillFactory/PYTHON-9. Библиотека NumPy/Collections Counter и defaultdict/ordereddict.py
@@ -6,3 +6,32 @@ print(ordered_client_ages)
 # OrderedDict([('Ivan', 19), ('Mark', 25), ('Andrey', 23), ('Maria', 20)])
 # OrderedDict([('Ivan', 19), ('Mark', 25), ('Andrey', 23), ('Maria', 20)])
 # OrderedDict([('Ivan', 19), ('Mark', 25), ('Andrey', 23), ('Maria', 20)])
+
+#Можно, например, отсортировать с помощью функции sorted список кортежей при создании из него OrderedDict, 
+#и объекты будут добавлены в порядке сортировки:
+
+data = [('Ivan', 19),('Mark', 25),('Andrey', 23),('Maria', 20)]
+# Сортируем по второму значению из кортежа, то есть по возрасту
+ordered_client_ages = OrderedDict(sorted(data, key=lambda x: x[1]))
+print(ordered_client_ages)
+# OrderedDict([('Ivan', 19), ('Maria', 20), ('Andrey', 23), ('Mark', 25)])
+
+#Если теперь добавить нового человека в словарь, новая запись окажется в конце:
+
+ordered_client_ages['Nikita'] = 18
+print(ordered_client_ages)
+# OrderedDict([('Ivan', 19), ('Maria', 20), ('Andrey', 23), ('Mark', 25), ('Nikita', 18)])ordered_client_ages['Nikita'] = 18
+
+#Если удалить элемент, а затем добавить его снова, он также окажется в конце:
+
+del ordered_client_ages['Andrey']
+print(ordered_client_ages)
+# OrderedDict([('Ivan', 19), ('Mark', 25), ('Maria', 20), ('Nikita', 18)])
+ordered_client_ages['Andrey'] = 23
+print(ordered_client_ages)
+# OrderedDict([('Ivan', 19), ('Mark', 25), ('Maria', 20), ('Nikita', 18), ('Andrey', 23)])
+
+import sys
+print(sys.version)
+# 3.4.3 (default, Nov 12 2018, 22:25:49)
+# [GCC 4.8.4]
